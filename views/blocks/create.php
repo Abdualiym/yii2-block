3b548bc6d2a4a3dd7c0f62c603b81ddf1f6bcb3e
abdualiym <a.abdualiym@gmail.com>
2019-05-09 13:53:37 +0500

    fix files

---
+++ views/blocks/create.php
@@ -0,0 +1,17 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $model abdualiym\text\forms\TextForm */
+
+$this->title = 'Добавить';
+$this->params['breadcrumbs'][] = ['label' => Yii::t('text', $page ? 'Pages' : 'Articles'), 'url' => ['index', 'page' => $page]];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="category-create">
+
+    <?= $this->render('_form', [
+        'model' => $model,
+        'page' => $page,
+    ]) ?>
+
+</div>
---
+++ views/blocks/index.php
@@ -0,0 +1,83 @@
+<?php
+
+use abdualiym\menu\components\MenuSlugHelper;
+use abdualiym\languageClass\Language;
+use abdualiym\text\entities\Text;
+use yii\grid\GridView;
+use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
+use abdualiym\text\entities\CategoryTranslation;
+use abdualiym\text\forms\TextForm;
+
+/* @var $this yii\web\View */
+/* @var $searchModel abdualiym\text\forms\TextSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+
+$this->title = Yii::t('text', $page ? 'Pages' : 'Articles');
+$this->params['breadcrumbs'][] = $this->title;
+$columns = [];
+if (!$page) {
+    $columns[] = [
+        'value' => function (Text $model) {
+            return $model->mainPhoto ? Html::img($model->mainPhoto->getThumbFileUrl('file', 'admin')) : null;
+        },
+        'format' => 'raw',
+        'contentOptions' => ['style' => 'width: 100px'],
+    ];
+    $columns[] =
+        [
+            'attribute' => 'category_id',
+            'label' => Yii::t('text', 'Category'),
+            'value' => function (Text $model) {
+                return $model->category ? $model->category->translations[0]['name'] : 'No';
+            },
+            'filter' => (new TextForm())->categoriesList(true)
+        ];
+    $columns[] =
+        [
+            'attribute' => 'date',
+            'label' => Yii::t('text', 'Date'),
+            'format' => 'date',
+        ];
+
+}
+$columns[] = [
+    'attribute' => 'title',
+    'label' => 'Название',
+    'value' => function (Text $model) {
+        foreach ($model->translations as $translation) {
+            if ($translation['lang_id'] == (Language::getLangByPrefix('ru'))['id']) {
+                $translate = $translation;
+            }
+        }
+        return Html::a(Html::encode($translate['title']), ['view', 'id' => $model->id, 'page' => (Yii::$app->request->get('page') ? true : false)]);
+    },
+    'format' => 'raw',
+];
+$columns[] =
+    [
+        'attribute' => 'status',
+        'label' => Yii::t('text', 'Status'),
+        'value' => function (Text $model) {
+            return \abdualiym\text\helpers\TextHelper::statusLabel($model->status);
+        },
+        'format' => 'html',
+        'filter' => [Text::STATUS_ACTIVE => 'Актывные', Text::STATUS_DRAFT => 'Черновики']
+    ];
+?>
+<div class="user-index">
+
+    <p>
+        <?= Html::a(Html::tag('i','',['class' => 'fa fa-plus']).' Добавить', ['create', 'page' => $page], ['class' => 'btn btn-success btn-flat']) ?>
+    </p>
+
+    <div class="box">
+        <div class="box-body">
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'columns' => $columns]) ?>
+        </div>
+    </div>
+</div>
