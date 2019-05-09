01abd3a11bd6820eba172ca81d3250f41a141e05
abdualiym <a.abdualiym@gmail.com>
2019-05-09 14:25:30 +0500

    fix

--- Bootstrap.php
+++ Bootstrap.php
@@ -3,7 +3,6 @@
 namespace abdualiym\block;
 
 use yii\base\BootstrapInterface;
-use Yii;
 
 
 class Bootstrap implements BootstrapInterface
@@ -13,17 +12,24 @@ class Bootstrap implements BootstrapInterface
      */
     public function bootstrap($app)
     {
+//        if (!$app->hasModule('block')) {
+//            $app->setModule('block', [
+//                'class' => '@abdualiym\block\Module'
+//            ]);
+//        }
+
+
         // Add module I18N category.
         if (!isset($app->i18n->translations['block']) && !isset($app->i18n->translations['block*'])) {
             $app->i18n->translations['block'] = [
                 'class' => 'yii\i18n\PhpMessageSource',
-                'basePath' => '@abdualiym/block/messages',
+                'basePath' => __DIR__ . '/messages',
                 'sourceLanguage' => 'en',
                 'forceTranslation' => true,
             ];
         }
 
-        Yii::$app->controllerMap['block'] = [
+        $app->controllerMap['block'] = [
             'class' => 'abdualiym\block\controllers\BlockController',
         ];
     }
